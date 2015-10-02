26528ef624266e8d84bf12f7af3b3a28a504e4c2
Bart Vrancken <bart@abuse.io>
2015-10-02 20:56:37 +0200

    update new layout

--- src/Woody.php
+++ src/Woody.php
@@ -5,8 +5,6 @@ namespace AbuseIO\Parsers;
 use Ddeboer\DataImport\Reader;
 use Ddeboer\DataImport\Writer;
 use Ddeboer\DataImport\Filter;
-use Log;
-use ReflectionClass;
 
 class Woody extends Parser
 {
@@ -15,7 +13,7 @@ class Woody extends Parser
      */
     public function __construct($parsedMail, $arfMail)
     {
-        parent::__construct($parsedMail, $arfMail);
+        parent::__construct($parsedMail, $arfMail, $this);
     }
 
     /**
@@ -25,45 +23,37 @@ class Woody extends Parser
      */
     public function parse()
     {
-        // Generalize the local config based on the parser class name.
-        $reflect = new ReflectionClass($this);
-        $this->configBase = 'parsers.' . $reflect->getShortName();
-
-        Log::info(
-            get_class($this) . ': Received message from: ' .
-            $this->parsedMail->getHeader('from') . " with subject: '" .
-            $this->parsedMail->getHeader('subject') . "' arrived at parser: " .
-            config("{$this->configBase}.parser.name")
-        );
-
         $this->feedName = 'default';
 
         if ($this->isKnownFeed() && $this->isEnabledFeed() && $this->hasArfMail()) {
-            preg_match_all('/([\w\-]+): (.*)[ ]*\r?\n/', $this->arfMail['report'], $regs);
-            $report = array_combine($regs[1], $regs[2]);
+            if (preg_match_all('/([\w\-]+): (.*)[ ]*\r?\n/', $this->arfMail['report'], $regs)) {
+                $report = array_combine($regs[1], $regs[2]);
 
-            if ($this->hasRequiredFields($report) === true) {
-                // Event has all requirements met, filter and add!
-                if ($report['Feedback-Type'] != 'abuse') {
-                    return $this->failed(
-                        "Unabled to detect the report type from this notifier"
-                    );
+                if ($this->hasRequiredFields($report) === true) {
+                    // Event has all requirements met, filter and add!
+                    if ($report['Feedback-Type'] != 'abuse') {
+                        return $this->failed(
+                            "Unabled to detect the report type from this notifier"
+                        );
+                    }
+
+                    $report = $this->applyFilters($report);
+
+                    $report['evidence'] = $this->arfMail['evidence'];
+
+                    $this->events[] = [
+                        'source'        => config("{$this->configBase}.parser.name"),
+                        'ip'            => $report['Source-IP'],
+                        'domain'        => false,
+                        'uri'           => false,
+                        'class'         => config("{$this->configBase}.feeds.{$this->feedName}.class"),
+                        'type'          => config("{$this->configBase}.feeds.{$this->feedName}.type"),
+                        'timestamp'     => strtotime($report['Received-Date']),
+                        'information'   => json_encode($report),
+                    ];
                 }
-
-                $report = $this->applyFilters($report);
-
-                $report['evidence'] = $this->arfMail['evidence'];
-
-                $this->events[] = [
-                    'source'        => config("{$this->configBase}.parser.name"),
-                    'ip'            => $report['Source-IP'],
-                    'domain'        => false,
-                    'uri'           => false,
-                    'class'         => config("{$this->configBase}.feeds.{$this->feedName}.class"),
-                    'type'          => config("{$this->configBase}.feeds.{$this->feedName}.type"),
-                    'timestamp'     => strtotime($report['Received-Date']),
-                    'information'   => json_encode($report),
-                ];
+            } else {
+                $this->warningCount++;
             }
         }
 
